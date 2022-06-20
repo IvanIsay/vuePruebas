5a3e6962d2d32670f12435f5a5bd1a4110172f31
IvanIsay <102552151+IvanIsay@users.noreply.github.com>
2022-06-20 16:31:11 -0500

    Introducion, Componentes y DIrectivas

---
+++ Pract1/primeraApp/src/components/test.vue
@@ -0,0 +1,49 @@
+
+<template>
+
+<div>
+
+<!--  <div v-html= "myHtml"> </div>-->
+<div v-text= "title"> </div>
+
+<div v-if="showname">
+  <p> {{ user.nombre }}</p>
+</div>
+<div v-else>
+    <p> No se puede mostrar el Dato</p>
+</div>
+
+<hr>
+
+  <ul>
+    <li v-for="tarea in tareas">
+      {{ tarea.titulo}}
+    </li>
+  </ul>
+
+
+
+</div>
+
+</template>
+
+
+<script>
+  export default{
+    data(){
+      return{
+        title:'Soy el titulo JS',
+        user:{ nombre: 'ivan', app: 'Guerra'},
+        myHtml: '<h3> Objeto HTML desde el script</h3>',
+        showname: false,
+        tareas:[{titulo:'Practica1'},{titulo:'Practica2'},{titulo:'Practica3'}]
+
+      }
+    }
+  }
+</script>
+
+
+<style>
+
+</style>
